90dd6ca44821882b3fd78ffa9e69102279d49dcd
tamiresslelis <tamiresslelis@gmail.com>
2018-02-15 21:13:40 -0200

    cliente

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,29 @@
+class NegociacaoController{
+
+    constructor(){
+
+      let $ = document.querySelector.bind(document);
+      this._inputData = $("#data");
+      this._inputQuantidade = $("#quantidade");
+      this._inputValor = $("#valor");
+    }
+    //manipulando o DOM
+    adiciona(event){
+      event.preventDefault();
+
+      console.log(typeof(this._inputData.value));
+      let helper = new DateHelper();
+
+
+      let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value);,
+            this._inputQuantidade.value,
+            this._inputValor.value
+      );
+
+      console.log(negociacao);
+      console.log(helper.dataParaTexto(negociacao.data));
+
+      }
+
+}
