1dff53d759c136fda281b3c43cf41e654e4170e1
tamiresslelis <tamiresslelis@gmail.com>
2018-02-16 14:57:05 -0200

    throw new error and template string no controller

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,13 +1,16 @@
 class DateHelper{
+  constructor(){
+    throw new Error('Está classe DateHelper não pode ser instânciada');
+  }
 
-  dataParaTexto(data){
-    return data.getDate()
-        +'/'+ (data.getMonth()+1)
-        +'/'+ data.getFullYear();
+  static dataParaTexto(data){
+    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
 
   }
-  textoParaData(texto){
-      //arrow function
+    static textoParaData(texto){
+
+      if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+        throw new Error('Deve estar no formato aaaa-mm-dd');
       return new Date(texto.split('-').map((item, indice) =>  item - indice % 2 ));
 
     }
